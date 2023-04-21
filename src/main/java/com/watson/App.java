26bf52453c44a271f5e03f26d794667473a596c4
Huy Le <huyyyle@gmail.com>
2023-04-21 11:41:52 -0700

    SearchEngine more data

--- src/main/java/com/watson/App.java
+++ src/main/java/com/watson/App.java
@@ -7,7 +7,7 @@ public class App
     public static void main( String[] args ) throws Exception
     {
         SearchEngine engine = new SearchEngine("V1");
-        ArrayList<Document> documents = engine.search("The dominant paper in our nation's capital, it's among the top 10 U.S. papers in circulation", 10);
+        ArrayList<Document> documents = engine.searchV1("The dominant paper in our nation's capital, it's among the top 10 U.S. papers in circulation", 10);
         for (Document doc : documents) {
             System.out.println(doc.get("title"));
         }
