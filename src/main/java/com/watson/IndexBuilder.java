551eba7cc6871ce30062773f3ba633da4bc97dca
ChodeBusta <115305664+ChodeBusta@users.noreply.github.com>
2023-04-21 03:19:28 -0700

    Incorporates the wiki subsets

--- src/main/java/com/watson/IndexBuilder.java
+++ src/main/java/com/watson/IndexBuilder.java
@@ -13,25 +13,31 @@ import org.apache.lucene.store.FSDirectory;
 
 public class IndexBuilder {
     public static void main(String[] args) throws IOException {
-        buildIndexExample("index-example");
+        File dir = new File("dataset/wiki-subset-20140602");
+        File[] directoryListing = dir.listFiles();
+        if (directoryListing != null) {
+            for (File child : directoryListing) {
+                buildIndexExample(child.toString());
+            }
+        }
     }
 
     public static void buildIndexExample(String index_name) throws IOException {
-                // Create a new index in the directory. Make sure you create a new directory for each new index. 
-                // Make sure all indexes are in the same directory (indicies/)
-                Directory dir = FSDirectory.open(new File("indicies/" + index_name).toPath());
-                IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
-                IndexWriter writer = new IndexWriter(dir, config);
-        
-                // Use wikipedia parser to parse the wikipedia dump to documents
-                ArrayList<Document> documents= WikipediaParser.parse("dataset/wiki-example.txt");
-        
-                //add each document to the index
-                for (Document doc : documents) {
-                    writer.addDocument(doc);
-                }
-                //close index writer
-                writer.close();
+        // Create a new index in the directory. Make sure you create a new directory for each new index. 
+        // Make sure all indexes are in the same directory (indicies/)
+        Directory dir = FSDirectory.open(new File("indicies/" + index_name.split("\\\\")[2]).toPath());
+        IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+        IndexWriter writer = new IndexWriter(dir, config);
+
+        // Use wikipedia parser to parse the wikipedia dump to documents
+        ArrayList<Document> documents= WikipediaParser.parse(index_name);
+
+        //add each document to the index
+        for (Document doc : documents) {
+            writer.addDocument(doc);
+        }
+        //close index writer
+        writer.close();
     }
     
 }
